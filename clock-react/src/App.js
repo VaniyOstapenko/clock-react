1d98c076c1515b256a79498c6ba3be232808ceeb
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-02-05 22:13:33 +0300

    Clock-React

---
+++ clock-react/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import style from './style.module.css'
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const data = new Date()
+    const interval = setInterval(() => setTime(data), 1000)
+    return () => clearInterval(interval)
+
+  }, [time])
+
+
+  return (
+    <div className={style.wrapper}>
+      <div className={style.oval}></div>
+      <div className={style.clock}>
+        <h1>{time.getHours()}:{time.getMinutes()}:{time.getSeconds()}</h1>
+        <p>{time.getDate()}.{time.getMonth()}.{time.getFullYear()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
